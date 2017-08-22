82627b9c0239d597f35ee680f58af24deeed1e43
Ark <ark6476@gmail.com>
2017-08-22 09:25:03 -0700

    change swank communication style

--- swank.lisp
+++ swank.lisp
@@ -1,1 +1,7 @@
 (setf swank:*globally-redirect-io* T)
+
+
+;; Incudine settings
+;; Avoid problems in realtime because the default value is :SPAWN (each
+;; request in a separate thread)
+;;(setf swank:*communication-style* :sigio)
