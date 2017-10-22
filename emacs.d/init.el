eae7eb052e4cc78a0e09ef3b1e8c37d96664ccdc
Ark <arktheprogrammer@gmail.com>
2017-10-22 14:56:48 -0700

    buffer revert util

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -189,6 +189,15 @@
           (select-window first-win)
           (if this-win-2nd (other-window 1))))))
 
+(defun revert-all-buffers ()
+  "Refreshes all open buffers from their respective files."
+  (interactive)
+  (dolist (buf (buffer-list))
+    (with-current-buffer buf
+      (when (and (buffer-file-name) (file-exists-p (buffer-file-name)) (not (buffer-modified-p)))
+        (revert-buffer t t t) )))
+  (message "all buffers refreshed"))
+
 ;; use ace-window
 (use-package ace-window)
 
