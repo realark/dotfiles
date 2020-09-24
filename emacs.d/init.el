36883348ac8c59d29bce4dbe9e4b8fe9437dd028
Ark <arktheprogrammer@gmail.com>
2020-09-24 06:59:16 -0700

    use eshell for shell scripting interaction

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -867,7 +867,7 @@ EOF"
 The first two elements must be a 1:1 unique mapping of major-modes.")
   (setq interactive-perspectives
         (list (list "lisp-mode" "slime-repl-mode" #'slime)
-              (list "sh-mode" "term-mode" #'get-term)
+              (list "sh-mode" "term-mode" #'eshell)
               (list "groovye-mode" "inferior-groovy-mode" #'run-groovy)
               (list "java-mode" "inferior-groovy-mode" #'run-groovy)
               (list "sql-mode" "sql-interactive-mode" #'sql-postgres)))
